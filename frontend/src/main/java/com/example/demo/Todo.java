b2f9ca8a6ead6fc08138669c284c9b3098a12f3e
loasit00 <l.asch@t-online.de>
2023-06-19 10:51:14 +0200

    remove stuff

--- frontend/src/main/java/com/example/demo/Todo.java
+++ frontend/src/main/java/com/example/demo/Todo.java
@@ -52,30 +52,5 @@ public class Todo implements Serializable{
     public String toString() {
         return "Todo [todo=" + content + ", completed=" + completed + "]";
     }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((todo == null) ? 0 : todo.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Todo other = (Todo) obj;
-        if (todo == null) {
-            if (other.todo != null)
-                return false;
-        } else if (!todo.equals(other.todo))
-            return false;
-        return true;
-    }
     
 }
