79ec365aa3e170066c6930f197fc6055f375c17f
Louis Asch <l.asch@t-online.de>
2023-06-19 12:52:51 +0000

    updates mapping

--- frontend/src/main/java/com/example/demo/Frontend.java
+++ frontend/src/main/java/com/example/demo/Frontend.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -53,7 +54,7 @@ public class Frontend {
         return "redirect:/";
 	}
 
-    @PostMapping("/delete")
+    @DeleteMapping("/delete")
     public String deleteItem(@RequestParam String todo){
 
         WebClient
