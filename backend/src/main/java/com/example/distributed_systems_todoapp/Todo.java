72a01f32b08d2017ac64519dcebc540354a093b2
Louis Asch <l.asch@t-online.de>
2023-06-20 18:03:04 +0000

    updates model

--- backend/src/main/java/com/example/distributed_systems_todoapp/Todo.java
+++ backend/src/main/java/com/example/distributed_systems_todoapp/Todo.java
@@ -1,13 +1,17 @@
-package com.example.distributed_systems_todoapp;
+package hse.distributedSystems.LabBackEnd.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "todo")
+@Table(name = "todos")
 public class Todo {
 
-    @Id    
-    public String content;
+    @Id
+    private String content;
     public int priority = 2;
 
     public Todo() {}
@@ -16,7 +20,7 @@ public class Todo {
         this.content = content;
         this.priority  = priority ;
     }
-    
+
     public String getContent() {
         return content;
     }
@@ -32,4 +36,10 @@ public class Todo {
     public void setPriority (int priority ) {
         this.priority  = priority ;
     }
+
+    @Override
+    public String toString(){
+        return content;
+    }
+    
 }
